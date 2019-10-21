7f22ae7fcb5fd90100164a1d5285e5673936f583
Christiana Ade <c.ade92@gmail.com>
2019-10-21 11:45:05 -0700

    Update sensor end and start date

--- Val_01_cdecLocate.R
+++ Val_01_cdecLocate.R
@@ -16,6 +16,8 @@ require(tictoc)
 require(tidyverse)
 require(rgeos)
 require(spdplyr)
+require(CDECRetrieve)
+require(lubridate)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ####             START USER DEFINED VARIABLES       #### 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -32,12 +34,32 @@ s5box <- readOGR("./Data/Vector/s5t5_swath.shp")
 id <- str_c("27,","221,",sep = "|")
 
 #### OUTPUT FILES ####
+# 1) out directory for vector files
 outDir <- "./Data/Vector"
-outShp <- "cdec_turbidity_stations_s5t5"
+# 2) out directory for final cdec shapefile
+outShp <- "cdec_turbidity_stations_s5t5_oct21"
 
+#### USER DEFINED FUNCTIONS ####
+durFil <- function(df) {
+  if(nrow(df)> 1){
+    if("event" %in% df$duration){
+      df_fil <- df %>% filter(duration == "event")
+    } else {
+      df_fil <- df %>% filter(duration == "hourly")
+    } 
+  } else {
+    return(df)
+  }
+}
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ####             END USER DEFINED VARIABLES        ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#### 10/20
+# previous versions did not have ", after the numbers in id
+# a new line was added to remove stations that were not 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -46,11 +68,31 @@ outShp <- "cdec_turbidity_stations_s5t5"
 #### Only select turbidity stations from cdec ####
 cdecTurb <- cdecStat %>% 
   # from the sensors column  match either 27 or 221. #! this could have problems bec is not exact match
-  filter(str_detect(Sensors,id)) %>%
-  # add column that has which turbidity station a sensor has
-  mutate(turbNum = str_extract(Sensors, "(27)|(221)"),
-  # add a column for the turbidity units 27 = NTU, 221 = FNu
-         turbUnits = ifelse(turbNum == 27, "NTU","FNU"))
+  filter(str_detect(Sensors,id)) #%>%
+  # # add column that has which turbidity station a sensor has
+  # mutate(turbNum = str_extract(Sensors, "(27)|(221)"),
+  # # add a column for the turbidity units 27 = NTU, 221 = FNu
+  #        turbUnits = ifelse(turbNum == 27, "NTU","FNU"))
+
+#### Identify Start Date of the Turbidity Sensor ####
+# Station Ids
+staId <- cdecTurb$STA
+# query cdec datasets for information
+statInfo <- sapply(staId,cdec_datasets, keyword = "turbidity", USE.NAMES = T,simplify = F)
+# remove repeat entries for duration hourly and daily
+statInfo <- lapply(statInfo,durFil)
+# add a STA column and bind all rows
+statInfo <- mapply(cbind, statInfo, "STA"=staId, SIMPLIFY=F) %>% bind_rows() %>% filter(!duration == "daily")
+
+#### Bind Cdec Station Information tgoether and change column names ####
+cdecTurb <- cdecTurb %>% 
+  # join station information that includes the sensor start and end date, ntu/fnu. and number of sensor
+  left_join(statInfo) %>%
+  # rename columns
+  rename("turbNum" = sensor_number, turbUnits = sensor_units, sensStart = start, sensEnd = end) %>%
+  dplyr::select(-sensor_name) 
+# anything that has and end date of 10/21/2019 makes the sensors is still collecting data
+  #mutate(end = ifelse(end >= Sys.Date()))
 
 #### change to spatial points dataframe and project ####
 cdec.spdf <- SpatialPointsDataFrame(cdecTurb[, c("Longitude", "Latitude")],data = cdecTurb[1:ncol(cdecTurb)])
